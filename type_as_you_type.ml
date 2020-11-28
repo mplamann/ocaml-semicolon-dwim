41df194cfc3c7ad8904b21a6f50904f80a4e7463
Mitchell Plamann <mitchell.plamann@gmail.com>
2020-11-28 12:50:32 -0500

    Type inference via merlin sorta works

--- type_as_you_type.ml
+++ type_as_you_type.ml
@@ -1,6 +1,8 @@
 open! Core
 open! Async
+module Async_process = Process
 open! Import
+module Process = Async_process
 
 let minor_mode =
   Minor_mode.define_minor_mode
@@ -25,8 +27,40 @@ let previous_expr () =
     let expr_node = Tree_sitter.Node.get_prev_sibling node_at_point in
     let start = Tree_sitter.Node.start_byte expr_node in
     let end_ = Tree_sitter.Node.end_byte expr_node in
-    Some (Current_buffer.contents ~start ~end_ ())
+    Some (start, end_, Current_buffer.contents ~start ~end_ ())
   | _ -> None
+;;
+
+let merlin_type_region ~start ~end_ =
+  let command =
+    [ "server"
+    ; "type-expression"
+    ; "-protocol"
+    ; "sexp"
+    ; "-log-file"
+    ; "-"
+    ; "-filename"
+    ; Current_buffer.file_name_exn ()
+    ; "-position"
+    ; (let line, col =
+         Current_buffer.save_excursion Sync (fun () ->
+             Point.goto_char end_;
+             Point.line_number (), Point.column_number ())
+       in
+       sprintf "%d:%d" line col)
+    ; "-expression"
+    ; Current_buffer.contents ~start ~end_ () |> Text.to_utf8_bytes
+    ]
+  in
+  Background.don't_wait_for [%here] (fun () ->
+      message_s [%sexp { command : string list }];
+      Process.run_exn
+        ~prog:"/home/mplamann/.opam/default/bin/ocamlmerlin"
+        ~args:command
+        ~stdin:(Current_buffer.contents () |> Text.to_utf8_bytes)
+        ()
+      >>| message);
+  ()
 ;;
 
 let () =
@@ -40,7 +74,9 @@ let () =
       message_s [%sexp [%here]];
       match previous_expr () with
       | None -> ()
-      | Some expr -> message_s [%sexp (expr : Text.t)])
+      | Some (start, end_, expr) ->
+        message_s [%sexp (expr : Text.t)];
+        merlin_type_region ~start ~end_)
 ;;
 
 let () = provide ("type_as_you_type" |> Symbol.intern)
