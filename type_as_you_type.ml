d2cf21e67bb95dc3e69bbfa5b4ab74ea8a8b4af5
Mitchell Plamann <mitchell.plamann@gmail.com>
2020-11-28 13:14:37 -0500

    First actually working version

--- type_as_you_type.ml
+++ type_as_you_type.ml
@@ -31,36 +31,46 @@ let previous_expr () =
   | _ -> None
 ;;
 
-let merlin_type_region ~start ~end_ =
-  let command =
-    [ "server"
-    ; "type-expression"
-    ; "-protocol"
-    ; "sexp"
-    ; "-log-file"
-    ; "-"
-    ; "-filename"
-    ; Current_buffer.file_name_exn ()
-    ; "-position"
-    ; (let line, col =
-         Current_buffer.save_excursion Sync (fun () ->
-             Point.goto_char end_;
-             Point.line_number (), Point.column_number ())
-       in
-       sprintf "%d:%d" line col)
-    ; "-expression"
-    ; Current_buffer.contents ~start ~end_ () |> Text.to_utf8_bytes
-    ]
+let merlin_type_region ~buffer ~start ~end_ =
+  let command, stdin =
+    Current_buffer.set_temporarily Sync buffer ~f:(fun () ->
+        ( [ "server"
+          ; "type-expression"
+          ; "-protocol"
+          ; "sexp"
+          ; "-log-file"
+          ; "-"
+          ; "-filename"
+          ; Current_buffer.file_name_exn ()
+          ; "-position"
+          ; (let line, col =
+               Current_buffer.save_excursion Sync (fun () ->
+                   Point.goto_char end_;
+                   Point.line_number (), Point.column_number ())
+             in
+             sprintf "%d:%d" line col)
+          ; "-expression"
+          ; Current_buffer.contents ~start ~end_ () |> Text.to_utf8_bytes
+          ]
+        , Current_buffer.contents () |> Text.to_utf8_bytes ))
   in
-  Background.don't_wait_for [%here] (fun () ->
-      message_s [%sexp { command : string list }];
-      Process.run_exn
-        ~prog:"/home/mplamann/.opam/default/bin/ocamlmerlin"
-        ~args:command
-        ~stdin:(Current_buffer.contents () |> Text.to_utf8_bytes)
-        ()
-      >>| message);
-  ()
+  (* message_s [%sexp { command : string list }]; *)
+  let%map result =
+    Process.run_exn
+      ~prog:"/home/mplamann/.opam/default/bin/ocamlmerlin"
+      ~args:command
+      ~stdin
+      ()
+  in
+  (* message result; *)
+  let read_from_string = Funcall.("read-from-string" <: string @-> return value) in
+  let assoc = Funcall.("assoc" <: Symbol.t @-> value @-> return value) in
+  let read = read_from_string result in
+  (* message_s [%sexp [%here], (read : Value.t)]; *)
+  let read = Value.car_exn read in
+  (* let listp = Funcall.("listp" <: value @-> return bool) in *)
+  (* message_s [%sexp [%here], (read : Value.t), (listp read : bool)]; *)
+  assoc (Symbol.intern "value") read |> Value.cdr_exn |> Value.to_utf8_bytes_exn
 ;;
 
 let () =
@@ -71,12 +81,27 @@ let () =
     ~define_keys:[ keymap, ";" ]
     (fun () ->
       Point.insert ";";
-      message_s [%sexp [%here]];
+      (* message_s [%sexp [%here]]; *)
       match previous_expr () with
       | None -> ()
-      | Some (start, end_, expr) ->
-        message_s [%sexp (expr : Text.t)];
-        merlin_type_region ~start ~end_)
+      | Some (start, end_, _expr) ->
+        (* message_s [%sexp (expr : Text.t)]; *)
+        Background.don't_wait_for [%here] (fun () ->
+            match%bind
+              merlin_type_region ~buffer:(Current_buffer.get ()) ~start ~end_
+            with
+            | "unit Deferred.t" ->
+              Point.goto_char end_;
+              (* TODO: This doesn't work if there's whitespace between point and ; *)
+              Point.delete_forward_char_exn 1;
+              Point.insert " in";
+              (* TODO: This isn't right -- this code is async. The point might have advanced already. *)
+              let point = Point.marker_at () in
+              Point.goto_char start;
+              Point.insert "let%bind () = ";
+              Point.goto_char (point |> Marker.position |> Option.value_exn);
+              return ()
+            | _ -> return ()))
 ;;
 
 let () = provide ("type_as_you_type" |> Symbol.intern)
